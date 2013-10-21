fb2ca0d04443665506839c44f48d6c47534f22c2
Robert Wittek <robert.wittek@tuwien.ac.at>
2013-10-21 19:26:48 +0200

    create.sql changed

--- Coceso/src/db_files/create.sql
+++ Coceso/src/db_files/create.sql
@@ -5,6 +5,24 @@ CREATE SEQUENCE incidents_id_seq;
 CREATE SEQUENCE cocesopois_id_seq;
 CREATE SEQUENCE cases_id_seq;
 
+CREATE TABLE cocesopois (
+  id INTEGER NOT NULL DEFAULT nextval('cocesopois_id_seq'),
+  address VARCHAR (64) NOT NULL,
+  longitude DOUBLE PRECISION NOT NULL DEFAULT (0),
+  latitude DOUBLE PRECISION NOT NULL DEFAULT (0),
+  minimumUnits INTEGER NOT NULL DEFAULT (0),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE cases (
+  id INTEGER NOT NULL DEFAULT nextval('cases_id_seq'),
+  place INTEGER,
+  organiser VARCHAR (64) NOT NULL,
+  pax INTEGER,
+  PRIMARY KEY (id),
+  FOREIGN KEY (place) REFERENCES cocesopois ON DELETE SET NULL
+);
+
 CREATE TABLE units (
   id INTEGER DEFAULT nextval('units_id_seq'),
   aCase integer NOT NULL,
@@ -23,14 +41,6 @@ CREATE TABLE units (
   FOREIGN KEY (home) REFERENCES cocesopois ON DELETE SET NULL
 );
 
-CREATE TABLE crews (
-  units_id integer NOT NULL,
-  persons_id integer NOT NULL,
-  PRIMARY KEY (units_id, persons_id),
-  FOREIGN KEY (units_id) REFERENCES units ON DELETE CASCADE,
-  FOREIGN KEY (persons_id) REFERENCES persons ON DELETE CASCADE
-);
-
 CREATE TABLE persons (
   id INTEGER NOT NULL DEFAULT nextval('persons_id_seq'),
   activeCase INTEGER,
@@ -42,6 +52,30 @@ CREATE TABLE persons (
   hashedPW VARCHAR (64),
   PRIMARY KEY (id),
   FOREIGN KEY (activeCase) REFERENCES cases ON DELETE SET NULL
+);
+
+CREATE TABLE crews (
+  units_id integer NOT NULL,
+  persons_id integer NOT NULL,
+  PRIMARY KEY (units_id, persons_id),
+  FOREIGN KEY (units_id) REFERENCES units ON DELETE CASCADE,
+  FOREIGN KEY (persons_id) REFERENCES persons ON DELETE CASCADE
+);
+
+CREATE TABLE incidents (
+  id INTEGER NOT NULL DEFAULT nextval('incidents_id_seq'),
+  aCase INTEGER NOT NULL,
+  state VARCHAR (16) NOT NULL,
+  priority INTEGER NOT NULL,
+  blue BOOLEAN NOT NULL,
+  bo INTEGER,
+  ao INTEGER,
+  info VARCHAR (64),
+  caller VARCHAR (32),
+  PRIMARY KEY (id),
+  FOREIGN KEY (aCase) REFERENCES cases ON DELETE CASCADE,
+  FOREIGN KEY (bo) REFERENCES cocesopois ON DELETE SET NULL,
+  FOREIGN KEY (ao) REFERENCES cocesopois ON DELETE SET NULL
 );
 
 CREATE TABLE log (
@@ -62,39 +96,7 @@ CREATE TABLE log (
   FOREIGN KEY (uzer) REFERENCES persons ON DELETE NO ACTION
 );
 
-CREATE TABLE cases (
-  id INTEGER NOT NULL DEFAULT nextval('cases_id_seq'),
-  place INTEGER,
-  organiser VARCHAR (64) NOT NULL,
-  pax INTEGER,
-  PRIMARY KEY (id),
-  FOREIGN KEY (place) REFERENCES cocesopois ON DELETE SET NULL
-);
 
-CREATE TABLE cocesopois (
-  id INTEGER NOT NULL DEFAULT nextval('cocesopois_id_seq'),
-  address VARCHAR (64) NOT NULL,
-  longitude DOUBLE PRECISION NOT NULL DEFAULT (0),
-  latitude DOUBLE PRECISION NOT NULL DEFAULT (0),
-  minimumUnits INTEGER NOT NULL DEFAULT (0),
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE incidents (
-  id INTEGER NOT NULL DEFAULT nextval('incidents_id_seq'),
-  aCase INTEGER NOT NULL,
-  state VARCHAR (16) NOT NULL,
-  priority INTEGER NOT NULL,
-  blue BOOLEAN NOT NULL,
-  bo INTEGER,
-  ao INTEGER,
-  info VARCHAR (64),
-  caller VARCHAR (32),
-  PRIMARY KEY (id),
-  FOREIGN KEY (aCase) REFERENCES cases ON DELETE CASCADE,
-  FOREIGN KEY (bo) REFERENCES cocesopois ON DELETE SET NULL,
-  FOREIGN KEY (ao) REFERENCES cocesopois ON DELETE SET NULL
-);
 
 CREATE TABLE tasks (
   incident_id INTEGER NOT NULL,
