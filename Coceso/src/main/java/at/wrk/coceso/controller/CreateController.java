bab1e087b6fc16e284cfc225b15104327909c883
Robert Wittek <robert.wittek@tuwien.ac.at>
2013-11-29 16:13:04 +0100

    Another little Bugfix...

--- Coceso/src/main/java/at/wrk/coceso/controller/CreateController.java
+++ Coceso/src/main/java/at/wrk/coceso/controller/CreateController.java
@@ -136,7 +136,7 @@ public class CreateController {
 
         for(int i = from; i <= to; i++){
             unit.call = request.getParameter("call_pre")+i;
-            int dump = unitDao.add(unit);
+            unitDao.add(unit);
         }
 
         return "redirect:/edit";
