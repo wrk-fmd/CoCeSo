71fa63627adcb545dbfa92cc3e72232d98ef495f
heinrichmartin <heinrichmartin@users.sourceforge.net>
2014-01-02 11:58:24 +0100

    + fixed typo in sequence

--- Coceso/src/db_files/create_operator.sql
+++ Coceso/src/db_files/create_operator.sql
@@ -1,5 +1,5 @@
 BEGIN;
-INSERT INTO person (id, given_name, sur_name, dnr, contact) VALUES (nextval('persons_id_seq'), 'Vorname','Nachname',0000,'+436640000000');
-INSERT INTO operator (id, allowlogin,username,hashedpw) VALUES (currval('persons_id_seq'), 't', 'v.nachname','');
-INSERT INTO operator_role (operator_fk, role) VALUES (currval('persons_id_seq'), 'Root');
+INSERT INTO person (id, given_name, sur_name, dnr, contact) VALUES (nextval('person_id_seq'), 'Vorname','Nachname',0000,'+436640000000');
+INSERT INTO operator (id, allowlogin,username,hashedpw) VALUES (currval('person_id_seq'), 't', 'v.nachname','');
+INSERT INTO operator_role (operator_fk, role) VALUES (currval('person_id_seq'), 'Root');
 COMMIT;
