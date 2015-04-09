f2d2dac82f9d75388421a72c7088e5c46a5209d6
Daniel Rohr <daniel.rohr@w.roteskreuz.at>
2015-04-09 22:37:09 +0200

    Local caching for streetname db

--- coceso/src/main/java/at/wrk/coceso/service/point/ViennaAutocomplete.java
+++ coceso/src/main/java/at/wrk/coceso/service/point/ViennaAutocomplete.java
@@ -1,28 +1,26 @@
 package at.wrk.coceso.service.point;
 
-import at.wrk.coceso.service.csv.CsvParseException;
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.nio.charset.Charset;
 import org.apache.commons.csv.CSVRecord;
 import org.apache.log4j.Logger;
-import org.springframework.core.io.UrlResource;
+import org.springframework.core.io.Resource;
 
 public class ViennaAutocomplete extends CsvAutocomplete {
 
   private final static Logger LOG = Logger.getLogger(ViennaAutocomplete.class);
 
-  public ViennaAutocomplete() throws MalformedURLException {
-    init(new UrlResource("http://data.wien.gv.at/daten/geo?service=WFS&version=1.1.0&request=GetFeature"
-            + "&typeName=ogdwien:GEONAMENSVERZOGD&propertyName=STR_NAME,BEZLISTE&outputFormat=csv"),
-            Charset.forName("ISO-8859-1"), ',');
+  public ViennaAutocomplete(Resource source) {
+    // Source shoult point to
+    // http://data.wien.gv.at/daten/geo?service=WFS&version=1.1.0&request=GetFeature&typeName=ogdwien:GEONAMENSVERZOGD&propertyName=STR_NAME,BEZLISTE&outputFormat=csv
+    // or local cache
+    init(source, Charset.forName("ISO-8859-1"), ',');
   }
 
   @Override
   protected void parseRecord(CSVRecord record) {
     String street = record.get("STR_NAME").trim(),
-            districts = record.get("BEZLISTE").trim(),
-            key = street.toLowerCase();
+        districts = record.get("BEZLISTE").trim(),
+        key = street.toLowerCase();
     if (districts.length() > 0) {
       for (String district : (districts.split("\\|"))) {
         try {
