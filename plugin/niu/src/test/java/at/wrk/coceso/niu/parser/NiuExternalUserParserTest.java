2bdd032ac6993e6324417180ccd07db3bd54311c
Robert Wittek <r.m.wittek@gmail.com>
2018-02-11 20:38:46 +0100

    improve niu parser

--- plugin/niu/src/test/java/at/wrk/coceso/niu/parser/NiuExternalUserParserTest.java
+++ plugin/niu/src/test/java/at/wrk/coceso/niu/parser/NiuExternalUserParserTest.java
@@ -50,4 +50,15 @@ public class NiuExternalUserParserTest {
         ExternalUser expectedUser = new ExternalUser(new ExternalUserId(1234, "Mustermann", "Max"), ImmutableSet.of("+43 (600) 1234", "+43 (600) 1235"));
         assertThat(externalUsers, contains(expectedUser));
     }
+
+    @Test
+    public void headerWithInvalidEncoding_returnPartOfData() throws ImportException {
+        String validCsv = "Kategorie|DNr.|Anrede|Vorg.Titel|Nachname|Vorname|Weitere Vornamen|Nachg.Titel|Bereich|Straße|PLZ|Ort|Mitgliederstatus|Telefon geschäftlich|Telefon Privat|Telefon WRK|Handy privat|Handy geschxxxftlich|Handy WRK|e-mail privat|e-mail geschäftlich|e-mail WRK|Fax geschäftlich|Fax Privat|fax WRK|Notruf Pager WRK|Pager|Bereitschaft WRK|Tetra Funkgerät|\n" +
+                "NIU-Kontakt|1234|Herr||Mustermann|Max||MSc|WRK\\LV WIEN\\KHD|Mustergasse 1/2|1010|Wien|AKTIV|||+43 (600) 1234|+43 (600) 1234|+43 (600) 1235||||max.mustermann@here.local||||000300||+43 (600) 1234||\n";
+
+        Collection<ExternalUser> externalUsers = sut.parseExternalUsers(validCsv);
+
+        ExternalUser expectedUser = new ExternalUser(new ExternalUserId(1234, "Mustermann", "Max"), ImmutableSet.of("+43 (600) 1234"));
+        assertThat(externalUsers, contains(expectedUser));
+    }
 }
