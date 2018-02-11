2ffb6bce106b80bb35a847a4515a440e928fff1f
Robert Wittek <r.m.wittek@gmail.com>
2018-02-11 17:40:36 +0100

    add autowired unit listeners

--- main/service-impl/src/main/java/at/wrk/coceso/entityevent/impl/EntityEventFactoryImpl.java
+++ main/service-impl/src/main/java/at/wrk/coceso/entityevent/impl/EntityEventFactoryImpl.java
@@ -1,37 +1,62 @@
 package at.wrk.coceso.entityevent.impl;
 
-import at.wrk.coceso.entityevent.EntityEventHandler;
-import java.util.HashSet;
-import java.util.Set;
-import org.springframework.stereotype.Component;
 import at.wrk.coceso.entityevent.EntityEventFactory;
-import java.util.function.Function;
+import at.wrk.coceso.entityevent.EntityEventHandler;
+import at.wrk.coceso.entityevent.EntityEventListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Component
 class EntityEventFactoryImpl implements EntityEventFactory {
 
-  private final static Set<EntityEventHandler<? extends Object>> instances = new HashSet<>();
+    private final static Set<EntityEventHandler<?>> instances = new HashSet<>();
 
-  @Autowired
-  private SocketMessagingTemplate template;
+    private final SocketMessagingTemplate template;
+    private final Collection<EntityEventListener<?>> serviceEntityEventListeners;
 
-  @Override
-  public <T> EntityEventHandler<T> getEntityEventHandler(Class<T> type) {
-    for (EntityEventHandler<? extends Object> instance : instances) {
-      if (instance.matches(type)) {
-        return (EntityEventHandler<T>) instance;
-      }
+    @Autowired
+    public EntityEventFactoryImpl(
+            final SocketMessagingTemplate template,
+            final Collection<EntityEventListener<?>> serviceEntityEventListeners) {
+        this.template = template;
+        this.serviceEntityEventListeners = serviceEntityEventListeners;
     }
 
-    EntityEventHandler<T> instance = new EntityEventHandlerImpl<>(type);
-    instances.add(instance);
-    return instance;
-  }
+    @Override
+    public <T> EntityEventHandler<T> getEntityEventHandler(Class<T> type) {
+        EntityEventHandler<T> instance = null;
+        for (EntityEventHandler<?> existingInstance : instances) {
+            if (existingInstance.matches(type)) {
+                instance = (EntityEventHandler<T>) existingInstance;
+            }
+        }
 
-  @Override
-  public <T> WebSocketWriter<T> getWebSocketWriter(String url, Class<?> jsonView, Function<T, Integer> delete) {
-    return new WebSocketWriter<>(template, url, jsonView, delete);
-  }
+        if (instance == null) {
+            List<EntityEventListener<T>> supportedServiceListeners = fillterSupportedListeners(type);
+            instance = new EntityEventHandlerImpl<>(type, supportedServiceListeners);
+            instances.add(instance);
+        }
 
+        return instance;
+    }
+
+    private <T> List<EntityEventListener<T>> fillterSupportedListeners(final Class<T> type) {
+        return serviceEntityEventListeners
+                        .stream()
+                        .filter(entityEventListener -> entityEventListener.isSupported(type))
+                        .map(listener -> (EntityEventListener<T>) listener)
+                        .collect(Collectors.toList());
+    }
+
+    @Override
+    public <T> WebSocketWriter<T> getWebSocketWriter(String url, Class<?> jsonView, Function<T, Integer> delete) {
+        return new WebSocketWriter<>(template, url, jsonView, delete);
+    }
 }
--- main/service/src/main/java/at/wrk/coceso/entityevent/EntityEventListener.java
+++ main/service/src/main/java/at/wrk/coceso/entityevent/EntityEventListener.java
@@ -2,8 +2,9 @@ package at.wrk.coceso.entityevent;
 
 public interface EntityEventListener<T> {
 
-  public void entityChanged(T entity, int concern, int hver, int seq);
+    void entityChanged(T entity, int concern, int hver, int seq);
 
-  public void entityDeleted(int id, int concern, int hver, int seq);
+    void entityDeleted(int id, int concern, int hver, int seq);
 
+    boolean isSupported(Class<?> supportedClass);
 }
