9a2a296b791c1cb084320aadc632709234cefc2c
Robert Wittek <r.m.wittek@gmail.com>
2018-03-09 23:34:42 +0100

    #16 fix automatic re-connect on database connection loss

--- config/db/src/main/java/at/wrk/coceso/config/db/JpaConfigurer.java
+++ config/db/src/main/java/at/wrk/coceso/config/db/JpaConfigurer.java
@@ -1,8 +1,10 @@
 package at.wrk.coceso.config.db;
 
 import at.wrk.coceso.config.DbConfig;
+
 import java.util.Properties;
 import javax.persistence.EntityManagerFactory;
+
 import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,57 +23,69 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories("at.wrk.coceso")
 class JpaConfigurer {
 
-  @Bean
-  @Primary
-  public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
-    LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-    em.setDataSource(dataSource);
-    em.setPackagesToScan("at.wrk.coceso");
+    @Bean
+    @Primary
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("at.wrk.coceso");
 
-    em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-    em.setPersistenceUnitName("at.wrk.coceso");
-    em.setLoadTimeWeaver(new InstrumentationLoadTimeWeaver());
-    em.setJpaProperties(additionalProperties());
+        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        em.setPersistenceUnitName("at.wrk.coceso");
+        em.setLoadTimeWeaver(new InstrumentationLoadTimeWeaver());
+        em.setJpaProperties(additionalProperties());
 
-    return em;
-  }
+        return em;
+    }
 
-  @Bean(destroyMethod = "close")
-  @Primary
-  public DataSource dataSource(DbConfig config) {
-    DataSource dataSource = new DataSource();
-    dataSource.setDriverClassName(config.getDriver());
-    dataSource.setUrl(config.getUrl());
-    dataSource.setUsername(config.getUsername());
-    dataSource.setPassword(config.getPassword());
-    dataSource.setMaxActive(80);
-    dataSource.setMaxIdle(80);
-    return dataSource;
-  }
+    @Bean(destroyMethod = "close")
+    @Primary
+    public DataSource dataSource(DbConfig config) {
+        DataSource dataSource = new DataSource();
+        dataSource.setDriverClassName(config.getDriver());
+        dataSource.setUrl(config.getUrl());
+        dataSource.setUsername(config.getUsername());
+        dataSource.setPassword(config.getPassword());
 
-  @Bean
-  @Primary
-  public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
-    JpaTransactionManager transactionManager = new JpaTransactionManager();
-    transactionManager.setEntityManagerFactory(emf);
+        dataSource.setMaxActive(80);
+        dataSource.setMaxIdle(80);
 
-    return transactionManager;
-  }
+        dataSource.setTestWhileIdle(true);
+        dataSource.setTestOnReturn(true);
 
-  @Bean
-  public PersistenceAnnotationBeanPostProcessor annotationBean() {
-    return new PersistenceAnnotationBeanPostProcessor();
-  }
+        dataSource.setTestOnBorrow(true);
+        dataSource.setValidationQuery("SELECT 1");
+        dataSource.setValidationInterval(10000);
 
-  private Properties additionalProperties() {
-    Properties properties = new Properties();
-    properties.setProperty("hibernate.dialect", JsonPostgreSQLDialect.class.getName());
-    properties.setProperty("hibernate.hbm2ddl.auto", "validate");
-    properties.setProperty("hibernate.connection.charSet", "UTF-8");
-    properties.setProperty("hibernate.show_sql", "false");
-    properties.setProperty("hibernate.formate_sql", "false");
-    properties.setProperty("hibernate.archive.autodetection", "class, hbm");
-    return properties;
-  }
+        dataSource.setRemoveAbandoned(true);
+        dataSource.setRemoveAbandonedTimeout(20);
+
+        return dataSource;
+    }
+
+    @Bean
+    @Primary
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf);
+
+        return transactionManager;
+    }
+
+    @Bean
+    public PersistenceAnnotationBeanPostProcessor annotationBean() {
+        return new PersistenceAnnotationBeanPostProcessor();
+    }
+
+    private Properties additionalProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", JsonPostgreSQLDialect.class.getName());
+        properties.setProperty("hibernate.hbm2ddl.auto", "validate");
+        properties.setProperty("hibernate.connection.charSet", "UTF-8");
+        properties.setProperty("hibernate.show_sql", "false");
+        properties.setProperty("hibernate.formate_sql", "false");
+        properties.setProperty("hibernate.archive.autodetection", "class, hbm");
+        return properties;
+    }
 
 }
