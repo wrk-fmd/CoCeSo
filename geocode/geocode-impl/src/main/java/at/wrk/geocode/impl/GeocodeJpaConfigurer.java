a69c72d0579d45e0c880eca71162c82b65571526
Robert Wittek <r.m.wittek@gmail.com>
2018-03-09 23:32:17 +0100

    #16 fix automatic re-connect on database connection loss

--- geocode/geocode-impl/src/main/java/at/wrk/geocode/impl/GeocodeJpaConfigurer.java
+++ geocode/geocode-impl/src/main/java/at/wrk/geocode/impl/GeocodeJpaConfigurer.java
@@ -1,8 +1,10 @@
 package at.wrk.geocode.impl;
 
 import at.wrk.geocode.GeocodeConfig;
+
 import java.util.Properties;
 import javax.persistence.EntityManagerFactory;
+
 import org.apache.tomcat.jdbc.pool.DataSource;
 import org.hibernate.dialect.PostgreSQL9Dialect;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -19,49 +21,61 @@ import org.springframework.transaction.PlatformTransactionManager;
 @EnableJpaRepositories(value = "at.wrk.geocode", entityManagerFactoryRef = "geocodeEntityManagerFactory", transactionManagerRef = "geocodeTransactionManager")
 class GeocodeJpaConfigurer {
 
-  @Bean(name = "geocodeEntityManagerFactory")
-  public LocalContainerEntityManagerFactoryBean entityManagerFactory(@Qualifier("geocodeDataSource") DataSource dataSource) {
-    LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-    em.setDataSource(dataSource);
-    em.setPackagesToScan("at.wrk.geocode");
+    @Bean(name = "geocodeEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(@Qualifier("geocodeDataSource") DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("at.wrk.geocode");
 
-    em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-    em.setPersistenceUnitName("at.wrk.geocode");
-    em.setLoadTimeWeaver(new InstrumentationLoadTimeWeaver());
-    em.setJpaProperties(additionalProperties());
+        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        em.setPersistenceUnitName("at.wrk.geocode");
+        em.setLoadTimeWeaver(new InstrumentationLoadTimeWeaver());
+        em.setJpaProperties(additionalProperties());
 
-    return em;
-  }
+        return em;
+    }
 
-  @Bean(name = "geocodeDataSource", destroyMethod = "close")
-  public DataSource dataSource(GeocodeConfig config) {
-    DataSource dataSource = new DataSource();
-    dataSource.setDriverClassName(config.getDriver());
-    dataSource.setUrl(config.getUrl());
-    dataSource.setUsername(config.getUsername());
-    dataSource.setPassword(config.getPassword());
-    dataSource.setMaxActive(80);
-    dataSource.setMaxIdle(80);
-    return dataSource;
-  }
+    @Bean(name = "geocodeDataSource", destroyMethod = "close")
+    public DataSource dataSource(GeocodeConfig config) {
+        DataSource dataSource = new DataSource();
+        dataSource.setDriverClassName(config.getDriver());
+        dataSource.setUrl(config.getUrl());
+        dataSource.setUsername(config.getUsername());
+        dataSource.setPassword(config.getPassword());
 
-  @Bean(name = "geocodeTransactionManager")
-  public PlatformTransactionManager geocodeTransactionManager(@Qualifier("geocodeEntityManagerFactory") EntityManagerFactory emf) {
-    JpaTransactionManager transactionManager = new JpaTransactionManager();
-    transactionManager.setEntityManagerFactory(emf);
+        dataSource.setMaxActive(80);
+        dataSource.setMaxIdle(80);
 
-    return transactionManager;
-  }
+        dataSource.setTestWhileIdle(true);
+        dataSource.setTestOnReturn(true);
 
-  private Properties additionalProperties() {
-    Properties properties = new Properties();
-    properties.setProperty("hibernate.dialect", PostgreSQL9Dialect.class.getName());
-    properties.setProperty("hibernate.hbm2ddl.auto", "validate");
-    properties.setProperty("hibernate.connection.charSet", "UTF-8");
-    properties.setProperty("hibernate.show_sql", "false");
-    properties.setProperty("hibernate.formate_sql", "false");
-    properties.setProperty("hibernate.archive.autodetection", "class, hbm");
-    return properties;
-  }
+        dataSource.setTestOnBorrow(true);
+        dataSource.setValidationQuery("SELECT 1");
+        dataSource.setValidationInterval(10000);
+
+        dataSource.setRemoveAbandoned(true);
+        dataSource.setRemoveAbandonedTimeout(20);
+
+        return dataSource;
+    }
+
+    @Bean(name = "geocodeTransactionManager")
+    public PlatformTransactionManager geocodeTransactionManager(@Qualifier("geocodeEntityManagerFactory") EntityManagerFactory emf) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf);
+
+        return transactionManager;
+    }
+
+    private Properties additionalProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", PostgreSQL9Dialect.class.getName());
+        properties.setProperty("hibernate.hbm2ddl.auto", "validate");
+        properties.setProperty("hibernate.connection.charSet", "UTF-8");
+        properties.setProperty("hibernate.show_sql", "false");
+        properties.setProperty("hibernate.formate_sql", "false");
+        properties.setProperty("hibernate.archive.autodetection", "class, hbm");
+        return properties;
+    }
 
 }
