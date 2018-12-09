3c04978f3ac74f8699b7ac64dc63a265b5ff0b58
Robert Wittek <r.m.wittek@gmail.com>
2018-12-09 20:31:09 +0100

    fix NPE in geobroker point conversion

--- plugin/geobroker/src/main/java/at/wrk/coceso/plugin/geobroker/external/GeoBrokerPoints.java
+++ plugin/geobroker/src/main/java/at/wrk/coceso/plugin/geobroker/external/GeoBrokerPoints.java
@@ -3,17 +3,21 @@ package at.wrk.coceso.plugin.geobroker.external;
 import at.wrk.coceso.entity.point.Point;
 import at.wrk.coceso.plugin.geobroker.contract.GeoBrokerPoint;
 import at.wrk.geocode.LatLng;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
 
 final class GeoBrokerPoints {
+    private static final Logger LOG = LoggerFactory.getLogger(GeoBrokerPoints.class);
+
     private GeoBrokerPoints() {
     }
 
     static GeoBrokerPoint mapPoint(@Nullable final Point position) {
         GeoBrokerPoint point = null;
 
-        if (position != null) {
+        if (position != null && position.getCoordinates() != null) {
             LatLng coordinates = position.getCoordinates();
             point = new GeoBrokerPoint(coordinates.getLat(), coordinates.getLng());
         }
