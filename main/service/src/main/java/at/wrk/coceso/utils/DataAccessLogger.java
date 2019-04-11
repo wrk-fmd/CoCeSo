90e92bc71a96f20eb698050bcd481f7cabd768a0
Robert Wittek <r.m.wittek@gmail.com>
2019-04-11 22:08:17 +0200

    remove sensitive patient data from log files

--- main/service/src/main/java/at/wrk/coceso/utils/DataAccessLogger.java
+++ main/service/src/main/java/at/wrk/coceso/utils/DataAccessLogger.java
@@ -3,24 +3,24 @@ package at.wrk.coceso.utils;
 import at.wrk.coceso.entity.Concern;
 import at.wrk.coceso.entity.Patient;
 import at.wrk.coceso.entity.User;
-import java.util.Collection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Collection;
+
 public abstract class DataAccessLogger {
 
-  private final static Logger LOG = LoggerFactory.getLogger(DataAccessLogger.class);
+    private final static Logger LOG = LoggerFactory.getLogger(DataAccessLogger.class);
 
-  public static void logPatientAccess(Patient patient, User user) {
-    LOG.info("{}: Reading patient information {}", user, patient);
-  }
+    public static void logPatientAccess(final Patient patient, final User user) {
+        LOG.info("{}: Reading patient information of patient {}", user, patient);
+    }
 
-  public static void logPatientAccess(Collection<Patient> infos, Concern concern, User user) {
-    LOG.info("{}: Loaded patients for concern '{}', matched {}", user, concern, infos);
-  }
+    public static void logPatientAccess(final Collection<Patient> infos, final Concern concern, final User user) {
+        LOG.info("{}: Loaded patients for concern '{}', {} patients matched.", user, concern, infos == null ? -1 : infos.size());
+    }
 
-  public static void logPatientAccess(Collection<Patient> infos, Concern concern, String query, User user) {
-    LOG.info("{}: Searching patients for query '{}' in concern '{}', matched {}", user, query, concern, infos);
-  }
-
+    public static void logPatientAccess(final Collection<Patient> infos, final Concern concern, final String query, final User user) {
+        LOG.info("{}: Searching patients for query '{}' in concern '{}', {} patients matched.", user, query, concern, infos == null ? -1 : infos.size());
+    }
 }
