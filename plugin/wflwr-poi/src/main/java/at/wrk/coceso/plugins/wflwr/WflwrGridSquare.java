15f9cd9556ba528b0b72c2521b60b59571f2650a
Daniel Rohr <daniel.rohr@w.roteskreuz.at>
2019-04-20 20:35:14 +0200

    Update POIs for WFLWR 2019

--- plugin/wflwr-poi/src/main/java/at/wrk/coceso/plugins/wflwr/WflwrGridSquare.java
+++ plugin/wflwr-poi/src/main/java/at/wrk/coceso/plugins/wflwr/WflwrGridSquare.java
@@ -11,7 +11,8 @@ public class WflwrGridSquare extends GridSquarePoi {
 
   public WflwrGridSquare() {
     //super("WFLWR/Resselpark/", new GridSquare(48.201110, 16.366190, 'A', 1), new LatLng(0, 3.35857E-4), new LatLng(-2.24128E-4, 0), 'Z', 17);
-    super("WFLWR/Start/", new GridSquare(48.206717, 16.359191, 'A', 1), new LatLng(2.02280e-04, -1.23044e-04), new LatLng(8.21927e-05, 3.02818e-04), 'Z', 17);
+    //super("WFLWR/Start/", new GridSquare(48.206717, 16.359191, 'A', 1), new LatLng(2.02280e-04, -1.23044e-04), new LatLng(8.21927e-05, 3.02818e-04), 'Z', 17);
+    super("WFLWR/Rathaus/", new GridSquare(48.206243, 16.359009, 'A', 1), new LatLng(2.19486e-04, -1.35865e-04), new LatLng(9.19668e-05, 3.24254e-04), 'Y', 17);
   }
 
 }
