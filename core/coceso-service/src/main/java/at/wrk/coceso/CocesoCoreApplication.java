dd8db7aceba30bac86e30f92c833283fccd2858a
Daniel Rohr <daniel.rohr@w.roteskreuz.at>
2020-05-18 00:17:38 +0200

    Update service configuration

--- core/coceso-service/src/main/java/at/wrk/coceso/CocesoCoreApplication.java
+++ core/coceso-service/src/main/java/at/wrk/coceso/CocesoCoreApplication.java
@@ -3,11 +3,10 @@ package at.wrk.coceso;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "at.wrk")
 public class CocesoCoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CocesoCoreApplication.class, args);
     }
-
 }
