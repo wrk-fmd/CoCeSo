730d2b9d202a8317ba85120c42d2beda4bdcc5c9
Robert Wittek <r.m.wittek@gmail.com>
2020-06-16 21:28:58 +0200

    try without explicit security filter registration

--- config/web/src/main/java/at/wrk/coceso/config/web/WebAppInitializer.java
+++ config/web/src/main/java/at/wrk/coceso/config/web/WebAppInitializer.java
@@ -3,7 +3,6 @@ package at.wrk.coceso.config.web;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
-import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.FilterRegistration;
@@ -21,9 +20,9 @@ public class WebAppInitializer implements WebApplicationInitializer {
         encodingFilter.addMappingForUrlPatterns(null, true, "/*");
         encodingFilter.setAsyncSupported(true);
 
-        FilterRegistration.Dynamic securityFilter = context.addFilter("securityFilter", new DelegatingFilterProxy("springSecurityFilterChain"));
-        securityFilter.addMappingForUrlPatterns(null, false, "/*");
-        securityFilter.setAsyncSupported(true);
+//        FilterRegistration.Dynamic securityFilter = context.addFilter("securityFilter", new DelegatingFilterProxy("springSecurityFilterChain"));
+//        securityFilter.addMappingForUrlPatterns(null, false, "/*");
+//        securityFilter.setAsyncSupported(true);
 
         ServletRegistration.Dynamic registration = context.addServlet("dispatcher", new DispatcherServlet(rootContext));
         registration.setLoadOnStartup(1);
