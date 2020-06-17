a7fdb7caeb5925ae8375bbe958f87af39727d8f0
Daniel Rohr <daniel.rohr@w.roteskreuz.at>
2020-06-17 17:43:58 +0200

    Update STOMP authorization handling

--- core/coceso-service/src/main/java/at/wrk/coceso/config/CocesoStompSecurityConfig.java
+++ core/coceso-service/src/main/java/at/wrk/coceso/config/CocesoStompSecurityConfig.java
@@ -1,13 +1,27 @@
 package at.wrk.coceso.config;
 
-import at.wrk.coceso.dto.CocesoExchangeNames;
+import static at.wrk.coceso.auth.AccessLevel.CONCERN_READ;
+import static at.wrk.coceso.auth.AccessLevel.CONTAINER_READ;
+import static at.wrk.coceso.auth.AccessLevel.INCIDENT_READ;
+import static at.wrk.coceso.auth.AccessLevel.MESSAGE_READ;
+import static at.wrk.coceso.auth.AccessLevel.PATIENT_READ;
+import static at.wrk.coceso.auth.AccessLevel.UNIT_READ;
+import static at.wrk.coceso.dto.CocesoExchangeNames.STOMP_CONCERNS;
+import static at.wrk.coceso.dto.CocesoExchangeNames.STOMP_CONTAINERS;
+import static at.wrk.coceso.dto.CocesoExchangeNames.STOMP_INCIDENTS;
+import static at.wrk.coceso.dto.CocesoExchangeNames.STOMP_MESSAGES;
+import static at.wrk.coceso.dto.CocesoExchangeNames.STOMP_PATIENTS;
+import static at.wrk.coceso.dto.CocesoExchangeNames.STOMP_UNITS;
+
 import at.wrk.fmd.mls.stomp.config.StompSecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
 import org.springframework.security.access.PermissionEvaluator;
-import org.springframework.security.access.expression.AbstractSecurityExpressionHandler;
+import org.springframework.security.access.expression.SecurityExpressionHandler;
 import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+import org.springframework.security.messaging.access.expression.DefaultMessageSecurityExpressionHandler;
 
 /**
  * This class configures the security policy of the STOMP endpoint.
@@ -22,16 +36,20 @@ class CocesoStompSecurityConfig extends StompSecurityConfig {
         this.evaluator = evaluator;
     }
 
-    @Override
-    protected void customizeExpressionHandler(AbstractSecurityExpressionHandler<Message<Object>> handler) {
+    @Bean
+    public SecurityExpressionHandler<Message<Object>> messageSecurityExpressionHandler() {
+        DefaultMessageSecurityExpressionHandler<Object> handler = new DefaultMessageSecurityExpressionHandler<>();
         handler.setPermissionEvaluator(evaluator);
+        return handler;
     }
 
     @Override
-    protected MessageSecurityMetadataSourceRegistry customizeInbound(MessageSecurityMetadataSourceRegistry registry) {
-        return registry
-                .simpSubscribeDestMatchers(buildDestination(CocesoExchangeNames.STOMP_CONCERNS, "*"))
-                .access("hasPermission(null, T(at.wrk.coceso.auth.AccessLevel).CONCERN_READ)")
-                .anyMessage().permitAll();
+    protected void customizeInbound(MessageSecurityMetadataSourceRegistry registry) {
+        hasPermission(registry, STOMP_CONCERNS, CONCERN_READ);
+        hasPermission(registry, STOMP_INCIDENTS, INCIDENT_READ);
+        hasPermission(registry, STOMP_UNITS, UNIT_READ);
+        hasPermission(registry, STOMP_CONTAINERS, CONTAINER_READ);
+        hasPermission(registry, STOMP_PATIENTS, PATIENT_READ);
+        hasPermission(registry, STOMP_MESSAGES, MESSAGE_READ);
     }
 }
