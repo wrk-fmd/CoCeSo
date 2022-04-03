c6c14486f966d466c31beb38296a544462cb4faa
Robert Wittek <r.m.wittek@gmail.com>
2022-04-03 17:10:37 +0200

    fix JSP error in patadmin settings

--- main/entity/src/main/java/at/wrk/coceso/form/GroupIcon.java
+++ main/entity/src/main/java/at/wrk/coceso/form/GroupIcon.java
@@ -1,13 +1,20 @@
 package at.wrk.coceso.form;
 
-public class GroupIcon {
+import java.util.Objects;
+
+public class GroupIcon implements Comparable<GroupIcon> {
     private final String name;
 
     public GroupIcon(final String name) {
-        this.name = name;
+        this.name = Objects.requireNonNull(name, "Name of group icon must not be null.");
     }
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public int compareTo(final GroupIcon o) {
+        return name.compareTo(o.name);
     }
 }
